7ee61e175fcd4120f0943c0b70e10e318f6055c7
Krzysztof Stopa <stopa.krzysztof.k@gmail.com>
2022-06-25 23:10:15 +0200

    Added custom toolbar to Rich text component that enable to add a single material icon

--- src/icons.js
+++ src/icons.js
@@ -9,3 +9,12 @@ export const MaterialIcon = function(icon, color, size) {
         </SVG>
     )
 }
+
+export const buildBase64MaterialIcon = function(iconPath, color, size) {
+    //width="' + size + '" height="' + size + '"
+    const icon = '' +
+        '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24">' +
+            '<path fill="' + color + '" d="' + iconPath + '" />' +
+        '</svg>';  
+    return  btoa(icon);
+}
